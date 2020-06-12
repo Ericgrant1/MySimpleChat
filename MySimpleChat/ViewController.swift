bba6f2bb93ebe5750c4298f61314f7c8e84e65df
Eric Grant <senatorericgrant@gmail.com>
2020-06-12 11:45:19 +0700

    Initial Commit

---
+++ MySimpleChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MySimpleChat
+//
+//  Created by Eric Grant on 12.06.2020.
+//  Copyright © 2020 Eric Grant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
