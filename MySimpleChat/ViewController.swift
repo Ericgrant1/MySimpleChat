b0e60ed18336a6b65442c59f8e462119af4aa1f5
Eric Grant <senatorericgrant@gmail.com>
2020-06-12 15:19:23 +0700

    feat: install SwiftUI preview

--- MySimpleChat/ViewController.swift
+++ MySimpleChat/ViewController.swift
@@ -7,15 +7,35 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        view.backgroundColor = .blue
     }
-
-
 }
 
+struct ViewControllerProvider: PreviewProvider {
+    
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        
+        let viewController = ViewController()
+        
+        func makeUIViewController(context: Context) -> ViewController {
+            return viewController
+        }
+        
+        func updateUIViewController(_ uiViewController: ViewController, context: Context) {
+            
+        }
+        
+    }
+}
