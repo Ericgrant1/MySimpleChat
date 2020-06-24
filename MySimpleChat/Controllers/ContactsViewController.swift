36571fe2b95d35c7715100f8a9858ef218224bc4
Eric Grant <senatorericgrant@gmail.com>
2020-06-24 18:03:52 +0700

    feat: create Contacts View Controlle

---
+++ MySimpleChat/Controllers/ContactsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ContactsViewController.swift
+//  MySimpleChat
+//
+//  Created by Eric Grant on 24.06.2020.
+//  Copyright © 2020 Eric Grant. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ContactsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+    }
+}
