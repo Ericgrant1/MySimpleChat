9b99e7bd6f0719f514b24f114e395e93fd3c9796
Eric Grant <senatorericgrant@gmail.com>
2020-06-27 22:06:58 +0700

    feat: add SwiftUI Preview for active cell

--- MySimpleChat/Controllers/Cell/ActiveChatsCell.swift
+++ MySimpleChat/Controllers/Cell/ActiveChatsCell.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import SwiftUI
 
 protocol ConfigureCell {
     static var reuseId: String { get }
@@ -16,12 +17,16 @@ protocol ConfigureCell {
 
 class ActiveChatsCell: UICollectionViewCell, ConfigureCell {
     
+    // MARK: - Properties
+    
     static var reuseId: String = "ActiveCahtsCell"
     
     let friendImageView = UIImageView()
     let friendName = UILabel(text: "User Name", font: .laoSangamMN20(), textColor: .black)
     let lastMessage = UILabel(text: "How are you?", font: .laoSangamMN18(), textColor: .black)
     let gradientView = UIView()
+    
+    // MARK: - Lifecyrcle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -31,7 +36,30 @@ class ActiveChatsCell: UICollectionViewCell, ConfigureCell {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - Helpers
+    
     func configure(with value: MyChat) {
+        print("123")
+    }
+}
+
+// MARK: - SwiftUI
+
+struct ActiveChatsProvider: PreviewProvider {
+    
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
         
+        let tabBarVC = MainTabBarController()
+        
+        func makeUIViewController(context: Context) -> MainTabBarController {
+            return tabBarVC
+        }
+        
+        func updateUIViewController(_ uiViewController: MainTabBarController, context: Context) {
+        }
     }
 }
