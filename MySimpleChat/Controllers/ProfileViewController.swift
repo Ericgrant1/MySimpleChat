112b9967e89a6433cc448215d51131a2de5a31ef
Eric Grant <senatorericgrant@gmail.com>
2020-07-05 19:27:38 +0700

    feat: add Properties & Customize Element for Profile View

--- MySimpleChat/Controllers/ProfileViewController.swift
+++ MySimpleChat/Controllers/ProfileViewController.swift
@@ -12,8 +12,46 @@ import SwiftUI
 
 class ProfileViewController: UIViewController {
     
+    // MARK: - Properties
+    
+    let containerView = UIView()
+    let imageView = UIImageView(image: #imageLiteral(resourceName: "human3"), contentMode: .scaleAspectFill)
+    let nameLabel = UILabel(text: "Aron Freedman", font: .systemFont(ofSize: 21, weight: .light), textColor: .black)
+    let aboutMeLabel = UILabel(text: "You have the opportunity to chat with the best man in the World!", font: .systemFont(ofSize: 15, weight: .light), textColor: .black)
+    let userTextField = UITextField()
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        customizeElements()
+        configureUI()
+    }
+    
+    // MARK: - Helpers
+    
+    private func customizeElements() {
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        aboutMeLabel.translatesAutoresizingMaskIntoConstraints = false
+        userTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        aboutMeLabel.numberOfLines = 0
+        
+        containerView.backgroundColor = .mainWhite()
+        containerView.layer.cornerRadius = 28
+    }
+}
+
+// MARK: - Setup constraints
+
+extension ProfileViewController {
+    
+    private func configureUI() {
+        
     }
 }
 
@@ -27,10 +65,10 @@ struct ProfileViewControllerProvider: PreviewProvider {
     
     struct ContainerView: UIViewControllerRepresentable {
         
-        let ProfileVC = ProfileViewController()
+        let profileVC = ProfileViewController()
         
         func makeUIViewController(context: Context) -> ProfileViewController {
-            return ProfileVC
+            return profileVC
         }
         
         func updateUIViewController(_ uiViewController: ProfileViewController, context: Context) {
