fcb0914b962b012b266f88fa7e56e011c7be34c6
Eric Grant <senatorericgrant@gmail.com>
2020-07-05 22:42:26 +0700

    feat: implement bounds for Elements of Inserted Text Field

--- MySimpleChat/Extensions/InsertedTextField.swift
+++ MySimpleChat/Extensions/InsertedTextField.swift
@@ -41,6 +41,32 @@ class InsertedTextField: UITextField {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    // MARK: - Helpers
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 36, dy: 0)
+    }
+    
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 36, dy: 0)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 36, dy: 0)
+    }
+    
+    override func leftViewRect(forBounds bounds: CGRect) -> CGRect {
+        var rect = super.leftViewRect(forBounds: bounds)
+        rect.origin.x += 12
+        return rect
+    }
+    
+    override func rightViewRect(forBounds bounds: CGRect) -> CGRect {
+        var rect = super.rightViewRect(forBounds: bounds)
+        rect.origin.x += -12
+        return rect
+    }
 }
 
 // MARK: - SwiftUI
