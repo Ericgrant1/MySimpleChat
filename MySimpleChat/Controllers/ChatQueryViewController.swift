2b16591e5df3dd3a3fcf7f827b9400f92fbb692f
Eric Grant <senatorericgrant@gmail.com>
2020-07-06 23:47:40 +0700

    feat: add Properties & extension for Chat Query

--- MySimpleChat/Controllers/ChatQueryViewController.swift
+++ MySimpleChat/Controllers/ChatQueryViewController.swift
@@ -12,10 +12,40 @@ import SwiftUI
 
 class ChatQueryViewController: UIViewController {
     
+    // MARK: - Properties
+    
+    let containerView = UIView()
+    let imageView = UIImageView(image: #imageLiteral(resourceName: <#T##String#>), contentMode: .scaleAspectFill)
+    let nameLabel = UILabel(text: "Aron Freedman", font: .systemFont(ofSize: 21, weight: .light), textColor: .black)
+    let aboutMeLabel = UILabel(text: "You have the opportunity to start a new chat.", font: .systemFont(ofSize: 15, weight: .light), textColor: .black)
+    let acceptButton = UIButton(title: "ACCEPT",
+                                titleColor: .white,
+                                backgroundColor: .black,
+                                font: .laoSangamMN20(),
+                                isShadow: false,
+                                cornerRadius: 12)
+    let denyButton = UIButton(title: "DENY",
+                              titleColor: #colorLiteral(red: 0.8352941176, green: 0.2, blue: 0.2, alpha: 1),
+                              backgroundColor: .mainWhite(),
+                              font: .laoSangamMN20(),
+                              isShadow: false,
+                              cornerRadius: 12)
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .green
+    }
+}
+
+// MARK: - Setup constraints
+
+extension ChatQueryViewController {
+    
+    private func configureUI() {
+        
     }
 }
 
