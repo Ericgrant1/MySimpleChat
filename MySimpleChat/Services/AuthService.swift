5f22b480b582c63f7b4ae69e2df0b04eeb88f6ad
Eric Grant <senatorericgrant@gmail.com>
2020-07-08 00:51:27 +0700

    refactor: Services

---
+++ MySimpleChat/Services/AuthService.swift
@@ -0,0 +1,16 @@
+//
+//  AuthService.swift
+//  MySimpleChat
+//
+//  Created by Eric Grant on 08.07.2020.
+//  Copyright © 2020 Eric Grant. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+class AuthService {
+    
+    
+}
