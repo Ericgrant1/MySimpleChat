7c811017dd6e90f9793135361c24a658cb3d7c85
Eric Grant <senatorericgrant@gmail.com>
2020-07-09 23:12:37 +0700

    feat: add Email Validation

---
+++ MySimpleChat/Services/Validation.swift
@@ -0,0 +1,35 @@
+//
+//  Verification.swift
+//  MySimpleChat
+//
+//  Created by Eric Grant on 09.07.2020.
+//  Copyright © 2020 Eric Grant. All rights reserved.
+//
+
+import Foundation
+
+class Validation {
+    
+    static func isFieled(email: String?, password: String?, confirmPassword: String?) -> Bool {
+        guard let password = password,
+        let confirmPassword = confirmPassword,
+        let email = email,
+        password != "",
+        confirmPassword != "",
+            email != "" else {
+                return false
+        }
+        return true
+    }
+    
+    static func isSimpleEmail(_ email: String) -> Bool {
+        let emailRegEx = "^.+@.+\\..{2,}$"
+        return check(text: email, regEx: emailRegEx)
+    }
+    
+    private static func check(text: String, regEx: String) -> Bool {
+        let predicate = NSPredicate(format: "SELF MATCHES %@", regEx)
+        return predicate.evaluate(with: text)
+    }
+    
+}
