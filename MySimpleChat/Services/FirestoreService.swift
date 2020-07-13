50f5d65af1728a7fafdf345d5cdec86837c24adb
Eric Grant <senatorericgrant@gmail.com>
2020-07-13 23:43:55 +0700

    feat: add FirestoreService

---
+++ MySimpleChat/Services/FirestoreService.swift
@@ -0,0 +1,22 @@
+//
+//  FirestoreService.swift
+//  MySimpleChat
+//
+//  Created by Eric Grant on 13.07.2020.
+//  Copyright © 2020 Eric Grant. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestore
+
+class FirestoreService {
+    
+    static let shared = FirestoreService()
+    
+    let db = Firestore.firestore()
+    
+    private var usersRef: CollectionReference {
+        return db.collection("users")
+    }
+}
--- Podfile
+++ Podfile
@@ -11,5 +11,7 @@ target 'MySimpleChat' do
   	pod 'Firebase/Auth'
   	pod 'Firebase/Database'
 	pod 'Firebase/Storage'
+	pod 'Firebase/Firestore'
+
 
 end
