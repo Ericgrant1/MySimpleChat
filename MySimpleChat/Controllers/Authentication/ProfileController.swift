a7336ed66a34efeb32c219bce5321efbbf5f3c79
Eric Grant <senatorericgrant@gmail.com>
2020-07-18 02:55:01 +0700

    feat: add current name to Google Auth in Firebase

--- MySimpleChat/Controllers/Authentication/ProfileController.swift
+++ MySimpleChat/Controllers/Authentication/ProfileController.swift
@@ -38,6 +38,11 @@ class ProfileController: UIViewController {
     init(currentUser: User) {
         self.currentUser = currentUser
         super.init(nibName: nil, bundle: nil)
+        
+        if let username = currentUser.displayName {
+            fullnameTextField.text = username
+        }
+        // TODO set google image
     }
     
     required init?(coder: NSCoder) {
