c3cf353c71915daa6f1986752028eae061398d64
Eric Grant <senatorericgrant@gmail.com>
2020-07-20 23:45:15 +0700

    add Storage Service

---
+++ MySimpleChat/Services/StorageService.swift
@@ -0,0 +1,17 @@
+//
+//  StorageService.swift
+//  MySimpleChat
+//
+//  Created by Eric Grant on 20.07.2020.
+//  Copyright © 2020 Eric Grant. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import FirebaseAuth
+import FirebaseStorage
+
+class StorageService {
+    
+    static let shared = StorageService()
+}
